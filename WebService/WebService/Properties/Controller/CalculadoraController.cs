87ed414128ef19b314ebd31d79e01bf24f400729
Sergio <a2019112566@estudiantes.upsa.edu.bo>
2021-01-14 19:03:18 -0400

    Suma Get y Post

---
+++ WebService/WebService/Properties/Controller/CalculadoraController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebService.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class CalculadoraController : Controller
+    {
+        [HttpGet]
+        public int Sumag(int num1, int num2)
+        {
+            return num1+num2;
+        }
+        [HttpPost]
+        public int Sumap([FromHeader]int num1, [FromHeader]int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}

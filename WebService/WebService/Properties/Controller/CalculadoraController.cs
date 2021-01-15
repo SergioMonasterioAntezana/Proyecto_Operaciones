38f67217cb9b13dce514912f6525464ccd57361b
Sergio <a2019112566@estudiantes.upsa.edu.bo>
2021-01-14 21:32:00 -0400

    Resta Get y Post

--- WebService/WebService/Properties/Controller/CalculadoraController.cs
+++ WebService/WebService/Properties/Controller/CalculadoraController.cs
@@ -4,21 +4,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebService.Controllers
+namespace WebService.Properties.Controllers
 {
     [ApiController]
     [Route("api/[Controller]")]
     public class CalculadoraController : Controller
     {
         [HttpGet]
+        [Route("suma")]
         public int Sumag(int num1, int num2)
         {
             return num1+num2;
         }
         [HttpPost]
+        [Route("suma")]
         public int Sumap([FromHeader]int num1, [FromHeader]int num2)
         {
             return num1 + num2;
+        }
+
+
+        [HttpGet]
+        [Route("resta")]
+        public int Restag(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        [HttpPost]
+        [Route("resta")]
+        public int Restap([FromHeader] int num1, [FromHeader] int num2)
+        {
+            return num1 - num2;
         }
     }
 }
